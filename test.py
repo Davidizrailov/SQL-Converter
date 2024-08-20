eb76f9c1a8369b3c3deec7d0a68d1d174f1d3600
walidbenromdhane-ey <walid.ben.romdhane@ca.ey.com>
2024-08-20 17:42:38 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-status = "in_progress"
-i=0
-while (status != 'complete') and (status != "failed") :
-    i += 1
-    time.sleep(1)
-    print(status)
-    if i==3: status = "failed" 
-print(status)
