938759420b0399ab323dbec000452f1fd4706f15
davidizrailov <david.izrailov@ca.ey.com>
2024-08-27 09:53:45 -0400

    Merge branch 'explained-answers' of https://github.com/Davidizrailov/SQL-Converter into explained-answers

--- prompts.py
+++ prompts.py
@@ -1,25 +1,16 @@
 
 system_message_PLSQL = f"""
-You are an expert in all forms of SQL, with a focus on converting SQL code between different dialects. Your primary task today is to convert the provided SQL code from Oracle to Snowflake while maintaining its functionality and logic.
+You are an expert in converting code between different languages. Your primary task is to convert the provided SQL code from Oracle PL/SQL to Snowflake.
 
 Details:
 
-Source Dialect: Oracle PL SQL (commonly used in legacy databases for procedures)
+Source Dialect: Oracle PL/SQL (commonly used in legacy databases for procedures)
 Target Dialect: Snowflake (commonly used in modern data warehouses)
 Task: Convert the SQL code while preserving the original query's structure and logic. Adapt functions and keywords to suit the Snowflake dialect. Handle differences in data types or functions accurately.
-Output Length: Provide a complete conversion of the given SQL code without unnecessary elaboration.
+Output: Explanation of the translation steps and the input code and at the end the full completed translated code.
 
-Steps:
-
-Identify and adapt any Oracle-specific syntax and functions to their Snowflake equivalents.
-Ensure that the converted code maintains the same logic and functionality.
-Verify that all data types and functions are compatible with Snowflake.
-
-Priority: Ensure the converted SQL code is robust and error-free for enterprise-level usage.
-
-Instructions:
-Follow the specified steps for conversion.
-Use python 3.11. Don't use javascript or SQL use python for the language. Make sure to properly deal with the keyword "OUT" as it is an unsupported data type. I have provided examples and documentation about scripting python in Snowflake can be found in the vector store
+Only use python 3.8 and SQL in translated code. Make sure to properly deal with the keyword "OUT" as it is an unsupported data type. 
+I have provided examples and documentation about scripting python in Snowflake can be found in the vector store.
 
 """
 
@@ -83,11 +74,19 @@ Use Python for processing; avoid JavaScript.
 def generate_prompt_PLSQL(code):
     prompt = f"""
 
-        Below is the code I want you to convert from Oracle PL/SQL to Snowflake SQL. Provide me ONLY the converted code, ensuring it is compatible with Snowflake syntax. 
-        There should be no text other than the code. 
+        Below is some Oracle PL/SQL Code. Right now, only explain what the code is doing, step by step.
        
         Here is the code:
         {code}
+    """
+    return prompt
+
+def generate_prompt2_PLSQL(code_description):
+    prompt=f"""
+        Using the code description, write me a snowflake procedure which matches this description. 
+        Return only the the converted code, ensuring it is compatible with Snowflake syntax. 
+        There should be no text other than the code. Use python 3.8 rather than javascript. Make sure to add a handler. Call the procedure as well. Utilize the snowflake documentation file is needed.
+        code description: {code_description} 
     """
     return prompt
 
